5c5798882ecfc7713394c4911c1b93d8572f68a1
Allison Mackey <a.mackey693@gmail.com>
2020-04-24 13:59:36 -0700

    removes User Input class

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -1,6 +1,6 @@
 // Business Logic 
 export class Currency {
-  async usdExchange() {
+  static async usdExchange() {
     try { 
       let response = await fetch (`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`)
       let jsonifiedResponse;
@@ -22,34 +22,34 @@ export class Currency {
  
 }
 
-export class UserInfo extends Currency {
-  constructor(userInput, selectedRate) {
-    this.userInput = userInput;
-    this.selectedRate = selectedRate; 
-  }
-  rateExchange() {
-    let num = this.userInput; 
-    let rate = this.selectedRate; 
-    let cadRate = usdExchange.CAD * num;
-    let eurRate = usdExchange.EUR * num; 
-    let jpyRate = usdExchange.JPY * num; 
-    let mxnRate = usdExchange.MXN * num; 
-    let thbRate = usdExchange.THB * num;
+// export class UserInfo extends Currency {
+//   static constructor(userInput, selectedRate) {
+//     this.userInput = userInput;
+//     this.selectedRate = selectedRate; 
+//   }
+//   static rateExchange() {
+//     let num = this.userInput; 
+//     let rate = this.selectedRate; 
+//     let cadRate = usdExchange.CAD * num;
+//     let eurRate = usdExchange.EUR * num; 
+//     let jpyRate = usdExchange.JPY * num; 
+//     let mxnRate = usdExchange.MXN * num; 
+//     let thbRate = usdExchange.THB * num;
 
-    if (num === 0 || rate === " ") {
-      return this.newRates = "Please fill out form";
-    } else if (num > 0 && rate === "CAD") {
-      this.newRate = cadRate; 
-    } else if (num > 0 && rate === "EUR") {
-      this.newRate = eurRate; 
-    } else if (num > 0 && rate === "JPY") {
-      this.newRate = jpyRate; 
-    } else if (num > 0 && rate === "MXN") {
-      this.newRate = mxnRate; 
-    } else if (num > 0 && rate === "THB") {
-      this.newRate = thbRate; 
-    }
-    return this.newRate; 
-  }
-}
+//     if (num === 0 || rate === " ") {
+//       return this.newRates = "Please fill out form";
+//     } else if (num > 0 && rate === "CAD") {
+//       this.newRate = cadRate; 
+//     } else if (num > 0 && rate === "EUR") {
+//       this.newRate = eurRate; 
+//     } else if (num > 0 && rate === "JPY") {
+//       this.newRate = jpyRate; 
+//     } else if (num > 0 && rate === "MXN") {
+//       this.newRate = mxnRate; 
+//     } else if (num > 0 && rate === "THB") {
+//       this.newRate = thbRate; 
+//     }
+//     return this.newRate; 
+//   }
+// }
 
--- src/main.js
+++ src/main.js
@@ -10,30 +10,37 @@ $(document).ready(function() {
     (async () => {
       let currency = new Currency();
       const usdExchange = await currency.usdExchange();
-      console.log(usdExchange);
-
-    
+      console.log(usdExchange);    
       
       let userInput = parseInt($('#insert-rate').val());
-      let newRate = parseInt$('#rate').val();));
-      let userInfo = new UserInfo (userInput); 
+      let newRate = $('#rate').val();
+      let userInfo = new UserInfo (userInput, newRate); 
+      let result = userInfo.rateExchange() 
 
-      userInfo.rateExchange() 
-
-      // function exchangeTo(userInput) {
-      //   if (userInput === " " || userInput < 1) {
-
-      //   } 
-      //   // let cadRate = usdExchange.CAD * userInput;
-      //   // let eurRate = usdExchange.EUR * userInput; 
-      //   // let jpyRate = usdExchange.EUR * userInput; 
-      //   // let 
-      // }
-     
-    })()
-
-
+      rateExchange() {
+        let num = userInput; 
+        let rate = newRate;
+        let cadRate = usdExchange.CAD * num;
+        let eurRate = usdExchange.EUR * num; 
+        let jpyRate = usdExchange.JPY * num; 
+        let mxnRate = usdExchange.MXN * num; 
+        let thbRate = usdExchange.THB * num;
     
+        if (num === 0 || rate === " ") {
+          return this.newRates = "Please fill out form";
+        } else if (num > 0 && rate === "CAD") {
+          this.newRate = cadRate; 
+        } else if (num > 0 && rate === "EUR") {
+          this.newRate = eurRate; 
+        } else if (num > 0 && rate === "JPY") {
+          this.newRate = jpyRate; 
+        } else if (num > 0 && rate === "MXN") {
+          this.newRate = mxnRate; 
+        } else if (num > 0 && rate === "THB") {
+          this.newRate = thbRate; 
+        }
+      }
+      $('#output').append(result)
+    })() 
   });
-
 });
